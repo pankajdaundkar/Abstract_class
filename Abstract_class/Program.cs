23e8aeb6539ec18a0591cedfd39173f0cfc5345e
pankajdaundkar <phd27993@gmail.com>
2023-06-23 12:58:47 +0530

    Add project files.

---
+++ Abstract_class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(5);
+            c1.CalculateArea();
+            Console.WriteLine(c1);
+
+            Rectangle r1 = new Rectangle(4, 5);
+            r1.CalculateArea();
+            Console.WriteLine(r1);
+        }
+    }
+}
